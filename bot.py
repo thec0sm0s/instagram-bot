250eb370ba4c73c15fa69d669e8148b9fc75698b
thecosmos <deepakrajko14@gmail.com>
2020-03-17 23:20:02 +0530

    Initial commit

---
+++ bot.py
@@ -0,0 +1,44 @@
+import time
+import configs
+
+from instabot import Bot
+
+
+class TheCosmos(Bot):
+
+    def __init__(self):
+        super().__init__()
+        self.login()
+
+    def login(self, **args):
+        super().login(username=configs.USERNAME, password=configs.PASSWORD)
+
+
+bot = TheCosmos()
+
+
+OPTIONS = [
+    ("1. Un-follow everyone.", bot.unfollow_everyone)
+]
+
+
+def show_menu():
+    print(f"{bot.username}\n{'-'*len(bot.username)}", end="\n\n")
+    for i in OPTIONS:
+        print(i[0])
+    print("\n")
+
+
+def log_info(info):
+    print(f"--- {info} ---")
+
+
+if __name__ == "__main__":
+    # while True:
+    #     show_menu()
+    #     choice = int(input(">> "))
+    #     start = time.time()
+    #     OPTIONS[choice - 1][1]()
+    #     log_info(f"Task finished in {time.time() - start} seconds.")
+
+    bot.unfollow_everyone()
---
+++ configs.py
@@ -0,0 +1,4 @@
+import os
+
+USERNAME = os.getenv("username")
+PASSWORD = os.getenv("password")
